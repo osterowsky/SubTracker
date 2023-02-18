3916b09103b315a05b14c7d2fdcaee98e70f6a20
Patryk Ostern <patryk.ostern@justwatch.com>
2023-02-18 21:04:19 +0100

    test

--- app.go
+++ app.go
@@ -7,7 +7,7 @@ import (
 
 func main() {
 	http.Handle("/", http.FileServer(http.Dir("./static")))
-	err := http.ListenAndServe(":8080", nil)
+	err := http.ListenAndServe(":8081", nil)
 	if err != nil {
 		log.Fatal(err)
 	}
