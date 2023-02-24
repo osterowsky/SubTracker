1879fac3b760a4b06eea0aec1c56e5702ec85ebe
Patryk Ostern <patryk.ostern@justwatch.com>
2023-02-24 08:02:01 +0100

    refactor table users

--- schema/v1.00__create_user_db.sql
+++ schema/v1.00__create_user_db.sql
@@ -1,6 +1,7 @@
+DROP TABLE IF EXISTS users;
 CREATE TABLE users (
     id SERIAL,
-    username VARCHAR(128),
+    username VARCHAR(128) UNIQUE,
     email VARCHAR(128) UNIQUE,
     passwordHash VARCHAR(32),
     PRIMARY KEY (id)
